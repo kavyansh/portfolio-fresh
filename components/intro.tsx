b9763c1cb0f76f2697fe90ce2d004bc495a59514
Yash_Srivastava <your_email@example.com>
2023-08-15 16:51:21 +0530

    icon change.

--- components/intro.tsx
+++ components/intro.tsx
@@ -32,7 +32,7 @@ export default function Intro() {
           >
             <Image
               src="/yash.jpeg"
-              alt="Ricardo portrait"
+              alt="Yash Profile Pic"
               width="192"
               height="192"
               quality="95"
