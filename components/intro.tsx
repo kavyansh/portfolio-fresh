f9f6408344fcaeaa022f4e758ee02ebed95eec04
Yash Srivastava <yash.srivastava@typeface.ai>
2024-06-18 22:58:05 +0530

    removed cv link

--- components/intro.tsx
+++ components/intro.tsx
@@ -137,7 +137,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="https://docs.google.com/document/d/1lMgspoJzvV08Kczwbp2jv2oQ_WhlQtJ4LweFB5cyksc/export?format=pdf"
+          href=""
           download
         >
           Download CV{" "}
