9cbd1f98026cd276c4b082c09f901dd71db1a171
Yash Srivastava <41780487+kavyansh@users.noreply.github.com>
2026-05-19 13:54:47 +0530

    Updated CV

--- components/intro.tsx
+++ components/intro.tsx
@@ -137,7 +137,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="https://docs.google.com/document/d/1ieL0eVp6dxY-m__r7e03THTTGMn4mkxOWt9go_alIf4/export?format=pdf"
+          href="https://docs.google.com/document/d/1O-InAtDYWdMMvEpnaw6NN6EbfMcipXSnbfhV2h0ONGk/export?format=pdf"
           download
         >
           Download CV{" "}
